5de61722e81424fccde203a84eaf3a25274a99de
cheddar <67003786+nealycaffrey@users.noreply.github.com>
2022-07-07 12:39:46 +0530

    added navigation

---
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { FlatList } from 'react-native';
+
+const ExpensesOutput = ({expenses}) => {
+  return (<View>
+    <FlatList />
+
+    LIST OF EXPENSES
+  </View>
+    
+  )
+}
+
+export default ExpensesOutput;
---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ExpensesSummary = () => {
+  return (
+    <View>
+        <Text>Last 7 Days</Text>
+        <Text>180 Rupees</Text>
+    </View>
+  )
+}
+
+export default ExpensesSummary;
